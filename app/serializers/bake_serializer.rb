cece66070ca01f470775dbc7401d1b768e8cc85c
learn-co-kat <twothistles@gmail.com>
2020-02-03 15:15:53 -0800

    Nested data appears correctly for each user. Can view bakes and notes associated with each bake. Two level nesting.

--- app/serializers/bake_serializer.rb
+++ app/serializers/bake_serializer.rb
@@ -1,3 +1,4 @@
 class BakeSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :total_flour_g, :total_flour_p, :water_g, :water_p, :salt_g, :salt_p, :leaven_g, :leaven_p, :hydration, :rating, :name, :date
+  has_many :notes
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,25 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password, :bakes
+  attributes :id, :email, :password
+  has_many :bakes
+  # has_many :notes, through: :bakes
+  ActiveModelSerializers.config.default_includes = '**'
 
-  def bakes
-    self.object.bakes.map do |bake|
-      {name: bake.name}
-  end 
+  # def bakes
+  #   self.object.bakes.map do |bake|
+  #     {name: bake.name,
+  #       total_flour_g: bake.total_flour_g,
+  #       total_flour_p: bake.total_flour_p,
+  #       water_g: bake.water_g,
+  #       water_p: bake.water_p,
+  #       salt_g: bake.salt_g,
+  #       salt_p: bake.salt_p,
+  #       leaven_g: bake.leaven_g,
+  #       leaven_p: bake.leaven_p,
+  #       hydration: bake.hydration,
+  #       rating: bake.rating,
+  #       date: bake.date
+  #     }
+  #   end 
+  # end 
+
 end
